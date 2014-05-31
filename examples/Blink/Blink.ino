ea9f9d79afa41db50427003e3e2c3fc5e765b21f
Michael Alyn Miller <malyn@strangeGizmo.com>
2014-05-31 09:54:00 -0700

    AVR and Arduino test app updates

--- examples/Blink/Blink.ino
+++ examples/Blink/Blink.ino
@@ -1,24 +1,125 @@
 #include <ARF.h>
 
-uint8_t arfDict[512];
-ARF arf(arfDict, sizeof(arfDict));
 
-int led = 13;
+// Macros
+#define LAST_FFI NULL
+
+#define ARF_EXTERN(name, fn, arity) \
+    static const char FFIDEF_ ## name ## _NAME[] PROGMEM = #name; \
+    static const arfFFI FFIDEF_##name PROGMEM = { LAST_FFI, FFIDEF_ ## name ## _NAME, arity, (void*)fn };
+
+#define GET_LAST_FFI(name) &FFIDEF_ ## name
+
+// Extern
+ARF_EXTERN(delay, delay, 2)
+#undef LAST_FFI
+#define LAST_FFI GET_LAST_FFI(delay)
+
+ARF_EXTERN(digitalWrite, digitalWrite, 2)
+#undef LAST_FFI
+#define LAST_FFI GET_LAST_FFI(digitalWrite)
+
+ARF_EXTERN(pinMode, pinMode, 2)
+#undef LAST_FFI
+#define LAST_FFI GET_LAST_FFI(pinMode)
+
+
+arfInt serialKeyQ()
+{
+  return Serial.available() > 0;
+}
+
+arfUnsigned serialKey()
+{
+  while (true)
+  {
+    int b = Serial.read();
+    if (b == -1)
+    {
+      continue;
+    }
+    else if (b == 0x0d)
+    {
+      return 0x0a;
+    }
+    else
+    {
+      return b;
+    }
+  }
+}
+
+void serialEmit(arfUnsigned ch)
+{
+  if (ch == 0x0a)
+  {
+    Serial.write(0x0d);
+  }
+
+  Serial.write(ch);
+}
 
 void setup()
 {
-  pinMode(led, OUTPUT);
-}
-
-int blink()
-{
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(1000);
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  arf.go(blink);
+  /* Initial dictionary with a couple of hand-coded definitions. */
+  unsigned char arfDict[512];
+  unsigned char * here = arfDict;
+
+  unsigned char * favnumLFA = here;
+  *here++ = 0x00; // LFAlo; bogus LFA offset
+  *here++ = 0x00; // LFAhi; bogus LFA offset
+  *here++ = 0x01; // DOCOLON
+  *here++ = 'F';
+  *here++ = 'A';
+  *here++ = 'V';
+  *here++ = 'N';
+  *here++ = 'U';
+  *here++ = 0x80 | 'M';
+  *here++ = 0x1a; // CHARLIT
+  *here++ = 27;
+  *here++ = 0x7f; // EXIT
+
+  unsigned char * twoxLFA = here;
+  *here++ = ((twoxLFA - favnumLFA)     ) & 0xff; // LFAlo
+  *here++ = ((twoxLFA - favnumLFA) >> 8) & 0xff; // LFAhi
+  *here++ = 0x01; // DOCOLON
+  *here++ = '2';
+  *here++ = 0x80 | 'X';
+  *here++ = 0x11; // DUP
+  *here++ = 0x13; // +
+  *here++ = 0x7f; // EXIT
+
+  unsigned char * delayLFA = here;
+  *here++ = ((delayLFA - twoxLFA)     ) & 0xff; // LFAlo
+  *here++ = ((delayLFA - twoxLFA) >> 8) & 0xff; // LFAhi
+  *here++ = 0x09; // DOFFI2
+  *here++ = ((uint16_t)&FFIDEF_delay     ) & 0xff; // FFIdef LSB
+  *here++ = ((uint16_t)&FFIDEF_delay >> 8) & 0xff; // FFIdef MSB
+
+  unsigned char * digitalWriteLFA = here;
+  *here++ = ((digitalWriteLFA - delayLFA)     ) & 0xff; // LFAlo
+  *here++ = ((digitalWriteLFA - delayLFA) >> 8) & 0xff; // LFAhi
+  *here++ = 0x09; // DOFFI2
+  *here++ = ((uint16_t)&FFIDEF_digitalWrite     ) & 0xff; // FFIdef LSB
+  *here++ = ((uint16_t)&FFIDEF_digitalWrite >> 8) & 0xff; // FFIdef MSB
+
+  unsigned char * pinModeLFA = here;
+  *here++ = ((pinModeLFA - digitalWriteLFA)     ) & 0xff; // LFAlo
+  *here++ = ((pinModeLFA - digitalWriteLFA) >> 8) & 0xff; // LFAhi
+  *here++ = 0x09; // DOFFI2
+  *here++ = ((uint16_t)&FFIDEF_pinMode     ) & 0xff; // FFIdef LSB
+  *here++ = ((uint16_t)&FFIDEF_pinMode >> 8) & 0xff; // FFIdef MSB
+
+  /* ARF VM */
+  ARF arf(arfDict, sizeof(arfDict), pinModeLFA - arfDict, here - arfDict,
+          LAST_FFI,
+          serialKeyQ, serialKey, serialEmit);
+
+  /* Launch the ARF interpreter. */
+  arf.go();
 }
--- test/main.cpp
+++ test/main.cpp
@@ -31,13 +31,54 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+/* -------------------------------------
+ * Includes.
+ */
+
+/* ANSI C includes. */
+#include <stdlib.h>
+
+/* AVR includes. */
 #include <avr/pgmspace.h>
 
+/* ARF includes. */
 #include "..\ARF.h"
+
+
+
+/* -------------------------------------
+ * Sample FFI definitions.
+ */
+
+// Macros
+#define LAST_FFI NULL
+
+#define ARF_EXTERN(name, fn, arity) \
+    static const char FFIDEF_ ## name ## _NAME[] PROGMEM = #name; \
+    static const arfFFI FFIDEF_##name PROGMEM = { LAST_FFI, FFIDEF_ ## name ## _NAME, arity, (void*)fn };
+
+#define GET_LAST_FFI(name) &FFIDEF_ ## name
+
+// Extern
+ARF_EXTERN(rand, rand, 0)
+#undef LAST_FFI
+#define LAST_FFI GET_LAST_FFI(rand)
+
+ARF_EXTERN(srand, srand, 1)
+#undef LAST_FFI
+#define LAST_FFI GET_LAST_FFI(srand)
+
+
+
+/* -------------------------------------
+ * ARF I/O primitives.
+ */
 
 unsigned int inputOffset = 0;
 static const uint8_t input[] PROGMEM = {
     '2', '7', ' ', '5', '4', ' ', '+', ' ', '.', '\n'
+    //'f','a','v','n','u','m', ' ', '2','x', ' ', '+', ' ', '.', '\n'
+    //'r','a','n','d', ' ', '.', '\n'
 };
 
 static arfInt arfStaticKeyQuestion(void)
@@ -66,10 +107,61 @@ static void arfStaticEmit(arfUnsigned ch)
     lastCh = ch;
 }
 
-uint8_t arfDict[512];
-ARF arf(arfDict, sizeof(arfDict), arfStaticKeyQuestion, arfStaticKey, arfStaticEmit);
+
+
+/* -------------------------------------
+ * main()
+ */
 
 int main(void)
 {
+    /* Initial dictionary with a couple of hand-coded definitions. */
+    unsigned char arfDict[512];
+    unsigned char * here = arfDict;
+
+    unsigned char * favnumLFA = here;
+    *here++ = 0x00; // LFAlo; bogus LFA offset
+    *here++ = 0x00; // LFAhi; bogus LFA offset
+    *here++ = 0x01; // DOCOLON
+    *here++ = 'F';
+    *here++ = 'A';
+    *here++ = 'V';
+    *here++ = 'N';
+    *here++ = 'U';
+    *here++ = 0x80 | 'M';
+    *here++ = 0x1a; // CHARLIT
+    *here++ = 27;
+    *here++ = 0x7f; // EXIT
+
+    unsigned char * twoxLFA = here;
+    *here++ = ((twoxLFA - favnumLFA)     ) & 0xff; // LFAlo
+    *here++ = ((twoxLFA - favnumLFA) >> 8) & 0xff; // LFAhi
+    *here++ = 0x01; // DOCOLON
+    *here++ = '2';
+    *here++ = 0x80 | 'X';
+    *here++ = 0x11; // DUP
+    *here++ = 0x13; // +
+    *here++ = 0x7f; // EXIT
+
+    unsigned char * randLFA = here;
+    *here++ = ((randLFA - twoxLFA)     ) & 0xff; // LFAlo
+    *here++ = ((randLFA - twoxLFA) >> 8) & 0xff; // LFAhi
+    *here++ = 0x07; // DOFFI0
+    *here++ = ((uint16_t)&FFIDEF_rand     ) & 0xff; // FFIdef LSB
+    *here++ = ((uint16_t)&FFIDEF_rand >> 8) & 0xff; // FFIdef MSB
+
+    unsigned char * srandLFA = here;
+    *here++ = ((srandLFA - randLFA)     ) & 0xff; // LFAlo
+    *here++ = ((srandLFA - randLFA) >> 8) & 0xff; // LFAhi
+    *here++ = 0x08; // DOFFI1
+    *here++ = ((uint16_t)&FFIDEF_srand     ) & 0xff; // FFIdef LSB
+    *here++ = ((uint16_t)&FFIDEF_srand >> 8) & 0xff; // FFIdef MSB
+
+    /* ARF VM */
+    ARF arf(arfDict, sizeof(arfDict), srandLFA - arfDict, here - arfDict,
+            LAST_FFI,
+            arfStaticKeyQuestion, arfStaticKey, arfStaticEmit);
+
+    /* Launch the ARF interpreter. */
     arf.go();
 }
